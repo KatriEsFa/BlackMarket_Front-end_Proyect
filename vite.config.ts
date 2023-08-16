16ac6157d090319ebbd941f735f119e537595605
KatriEsFa <katriesfa@gmail.com>
2023-08-16 15:28:53 +1000

    Sass has been added. Routes shortcuts has been created, its a previous step for absolute routes

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,33 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import sass from 'sass';
 
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  css: {
+    preprocessorOptions: {
+      scss: {
+        implementation: sass,
+      },
+    },
+  },
   resolve: {
     alias: {
       '@src': './src',
-      '@routes': './src/routes'
-
+      '@adapters': './src/adapters',
+      '@components': './src/components',
+      '@hooks': './src/hooks',
+      '@interceptors': './src/interceptors',
+      '@layout': './src/layout',
+      '@models': './src/models',
+      '@pages': './src/pages',
+      '@redux': './src/redux',
+      '@routes': './src/routes',
+      '@services': './src/services',
+      '@styledComponents': './src/styled-components',
+      '@utilities': './src/utilities',
     }
   }
 })
